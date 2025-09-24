b7e21defadba9ae6b16d9565f9d083d12a82cc35
Venkatesh <147642003+narlavenkatesh77@users.noreply.github.com>
2025-09-24 14:57:37 +0530

    Update tenantMiddleware.js

--- middlewares/tenantMiddleware.js
+++ middlewares/tenantMiddleware.js
@@ -5,6 +5,7 @@ export default async function tenantMiddleware(req, res, next) {
     const tenantId = req.headers["x-tenant-id"]; // client must send tenant ID
     if (!tenantId) return res.status(400).json({ error: "Tenant ID missing" });
 
+    // req.tenantDb is important
     req.tenantDb = await getTenantDb(tenantId);
     next();
   } catch (err) {
